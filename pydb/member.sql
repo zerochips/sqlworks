b9e1d778f70d568d9e064f262ffd0d56309ac26e
zerochips <zerochips123@gmail.com>
2023-05-03 18:19:16 +0900

    학원

---
+++ pydb/member.sql
@@ -0,0 +1,17 @@
+CREATE TABLE member(
+	memberid	TEXT PRIMARY KEY,
+	passwd		TEXT NOT NULL,
+	name		TEXT NOT NULL,
+	gender		TEXT,
+	regdate		DATETIME DEFAULT (datetime('now','localtime'))
+);
+
+INSERT INTO member (memberid, passwd, name, gender) 
+VALUES ('colud123','m123456#','장그래','남');
+
+INSERT INTO member (memberid, passwd, name, gender) 
+VALUES ('colud456','m123456#','오대리','여');
+
+DELETE FROM member;
+
+SELECT * FROM member;

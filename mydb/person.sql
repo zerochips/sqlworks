fcc49b1ffaaa038cd381792a63f5346779f8517a
zerochips <zerochips123@gmail.com>
2023-05-25 18:21:42 +0900

    오라클 자바 테이블 클래스 연동

---
+++ mydb/person.sql
@@ -0,0 +1,14 @@
+-- java 연동 테이블 생성
+CREATE TABLE person(
+    userid  VARCHAR2(10) PRIMARY KEY,
+    userpw  VARCHAR2(10) NOT NULL,
+    name    VARCHAR2(20) NOT NULL,
+    age     NUMBER(3)
+);
+
+INSERT INTO person(userid, userpw, name, age)
+VALUES ('cloud', 'cloud123', '구름이', 120);
+
+SELECT * FROM person;
+
+COMMIT;

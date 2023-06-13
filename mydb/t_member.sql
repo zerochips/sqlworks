69ec3e9b8bdc9e26efb975c02224594ac1a8e1a8
zerochips <zerochips123@gmail.com>
2023-06-13 17:16:01 +0900

    t_board

--- mydb/t_member.sql
+++ mydb/t_member.sql
@@ -16,14 +16,14 @@ CREATE TABLE t_board(
     hit         NUMBER DEFAULT 0,
     memberid    VARCHAR2(20) NOT NULL,
     CONSTRAINT FK_MemberBoard FOREIGN KEY(memberid)
-    REFERENCES t_member(memberid)
+    REFERENCES t_member(memberid) ON DELETE CASCADE
 );
 
 -- 글번호, 글제목, 글내용, 작성일, 수정일, 조회수, 회원아이디(외래키)
 -- 자동 순번(SEQUENCE)
 CREATE SEQUENCE b_seq;
 
--- drop table t_member;
+
 
 -- joindate는 안쓰는게 맞습니다.
 INSERT INTO t_member (memberid, passwd, name, gender) VALUES ('cloud', 'm12345', '김민정', '여');
@@ -34,5 +34,13 @@ VALUES (b_seq.NEXTVAL, '가입인사', '안녕하세요. 반갑습니다.', 'cloud');
 COMMIT;
 
 SELECT * FROM t_member;
-SELECT * FROM t_board;
+select * from t_board;
+
+-- drop table t_member;
+drop table t_board; -- board table 삭제
+drop sequence b_seq;   -- boar b_seq 삭제
+
+-- 새로 고침 시 자동생성 오류 발생
+-- t_board와 sequence b_seq drop 삭제 후 다시 재 생성
+
 

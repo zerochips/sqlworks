09173fe9c1db172832826423d3cdd32689b37058
zerochips <zerochips123@gmail.com>
2023-06-16 19:56:28 +0900

    파일 업로드 칼럼 추가

--- mydb/t_member_board.sql
+++ mydb/t_member_board.sql
@@ -20,10 +20,17 @@ CREATE TABLE t_board(
 );
 
 -- 글번호, 글제목, 글내용, 작성일, 수정일, 조회수, 회원아이디(외래키)
--- 자동 순번(SEQUENCE)
-CREATE SEQUENCE b_seq;
+-- 자동 순번(SEQUENCE)전체코드(CACHE 기본값 20 - 캐시 사용안함)
+CREATE SEQUENCE b_seq
+INCREMENT BY 1
+START WITH 1
+MINVALUE 1
+MAXVALUE 9999
+NOCYCLE
+NOCACHE;
 
-
+-- 파일 업로드 칼럼 추가
+ALTER TABLE t_board ADD fileupload VARCHAR2(100);
 
 -- joindate는 안쓰는게 맞습니다.
 INSERT INTO t_member (memberid, passwd, name, gender) VALUES ('cloud', 'm12345', '김민정', '여');
@@ -32,15 +39,24 @@ INSERT INTO t_board(bnum, title, content, memberid)
 VALUES (b_seq.NEXTVAL, '가입인사', '안녕하세요. 반갑습니다.', 'cloud');
 
 COMMIT;
-
 SELECT * FROM t_member;
-select * from t_board;
+SELECT * FROM t_board;
+
+delete from t_member where name = '가미';
+
+-- id 중복 체크
+SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result
+FROM t_member WHERE memberid = 'cloud';
+
+
+
+
 
 -- drop table t_member;
-drop table t_board; -- board table 삭제
-drop sequence b_seq;   -- boar b_seq 삭제
+--drop table t_board; -- board table 삭제
+--drop sequence b_seq;   -- boar b_seq 삭제
 
 -- 새로 고침 시 자동생성 오류 발생
--- t_board와 sequence b_seq drop 삭제 후 다시 재 생성
+-- t_board와 sequence b_seq    -- drop 삭제 후 다시 재 생성
 
 
